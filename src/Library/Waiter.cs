9be2dd790389a58ccdac2607396f304559942b1e
OliverBillig <oliver.billig2007@gmail.com>
2026-08-19 10:03:28 -0300

    Commit de dish y waiter

--- src/Library/Waiter.cs
+++ src/Library/Waiter.cs
@@ -13,6 +13,24 @@ namespace Ucu.Poo.Restaurant
     /// </summary>
     public class Waiter
     {
+        public string Name {get; set;}
         private ArrayList assignedTables = new ArrayList();
+
+        public Waiter(string name)
+        {
+            Name = name;
+            assignedTables = new ArrayList();
+        }
+
+        public void AssignTable(Table table)
+        {
+            assignedTables.Add(table);
+        }
+
+        public void TakeOrder(Table table, Dish dish)
+        {
+            table.AddToOrder(dish);
+        }
+
     }
 }
